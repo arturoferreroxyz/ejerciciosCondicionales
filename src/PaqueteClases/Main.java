9f9574b5d52591ea2a4742d4dce34c15077c853e
quimerartech <arturoferrero@gmail.com>
2023-01-30 18:50:36 +0100

    commit

--- src/PaqueteClases/Main.java
+++ src/PaqueteClases/Main.java
@@ -12,6 +12,7 @@ import PaqueteClases.PaqueteInterfaz.Videojuego;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Bienvenido a ejercicios variados en java");
         //ejercicioObjetoPersona();
         //ejercicioObjetoPassword();
         //ejercicioSuperclaseElectrodomestico();
